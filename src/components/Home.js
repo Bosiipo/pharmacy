20ccb16c51a70818ecc83e23c9bd7194cdaeabbe
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-10 00:14:20 +0000

    add home screen

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Home.css";
+import Drug from "./Drug";
+
+function Home() {
+  return (
+    <div class="home">
+      <div className="home__container">
+        <img
+          src="https://images.pexels.com/photos/3882464/pexels-photo-3882464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt=""
+          className="home__image"
+        />
+
+        <div className="home__row">
+          <Drug />
+        </div>
+        <div className="home__row"> </div>
+        <div className="home__row"> </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

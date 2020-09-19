2a8cd21de9f2fb8ce1af6202ce6ba7522a56b8d0
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-19 18:33:39 +0000

    add subttal page

---
+++ src/components/Subtotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subtotal.css";
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <h2> Subtotal</h2>
+    </div>
+  );
+}
+
+export default Subtotal;

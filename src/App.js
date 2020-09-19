9966775b1465683270093746b39c843c1528d7ce
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-19 18:34:18 +0000

    add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header.js";
 import Home from "./components/Home";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
